e1e4f1bafa4284f0222d52ed367b9f4cdc0b81ea
julius ndakala <juliusndakala35@gmail.com>
2023-11-03 10:39:58 +0300

    created pages

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <ul className="nav-links">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
