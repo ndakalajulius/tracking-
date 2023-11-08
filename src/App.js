d13409efb6d0420780b5dbb4de071908f27173bd
julius ndakala <juliusndakala35@gmail.com>
2023-11-08 16:08:09 +0300

    added contact form

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
       </Routes>
     </Router>
     <Signup/>
+    <Contact/>
     </div>
   );
 }
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
-function Contact() {
+// Define styled components for your Contact Us page
+const ContactContainer = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+  text-align: center;
+`;
+
+const ContactHeading = styled.h1`
+  font-size: 24px;
+  color: #333;
+`;
+
+const ContactInfo = styled.div`
+  font-size: 18px;
+  color: #666;
+  margin-top: 10px;
+`;
+
+const ContactLink = styled.a`
+  text-decoration: none;
+  color: #007BFF;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Contact = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <ContactContainer>
+      <ContactHeading>Contact Us</ContactHeading>
+      <ContactInfo>
+        If you have any questions or need assistance, please feel free to
+        <ContactLink> contact us</ContactLink>.
+      </ContactInfo>
+    </ContactContainer>
+  );
+};
 
-export default Contact
+export default Contact;
